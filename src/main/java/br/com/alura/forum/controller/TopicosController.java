f89d04d5b140966e001191c39a9b124209510af2
fabianoheizer <fabianoheizer@gmail.com>
2022-06-14 09:13:09 -0300

    Criação do projeto

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,48 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping("")
+	public List<TopicoDto> lista(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder){
+		Topico topico = topicoForm.convert(cursoRepository);
+		topicoRepository.save(topico);
+
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+
+		// reposnde o 201 com a url do recurso criado e o body com o que foi criado
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+
+}
