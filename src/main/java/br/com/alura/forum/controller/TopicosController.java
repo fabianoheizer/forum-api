3b8b4c9973396d7873cd58578fda16fbbbc94f1c
fabianoheizer <fabianoheizer@gmail.com>
2022-07-02 22:49:57 -0300

    Atualizando paginacao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,8 +7,9 @@ import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -26,10 +27,8 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping("")
-	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtdPorPagina) {
-
-		Pageable paginacao = PageRequest.of(pagina, qtdPorPagina);
-
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
+								 @PageableDefault(page = 0, size = 10,  direction = Sort.Direction.ASC) Pageable paginacao) {
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDTO.converter(topicos);
