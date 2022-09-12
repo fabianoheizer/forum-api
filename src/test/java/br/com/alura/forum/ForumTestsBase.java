352df06731cbd0eb6266589d621ae8c25975eaa9
fabianoheizer <fabianoheizer@gmail.com>
2022-09-12 06:39:24 -0300

    Testes no projeto

---
+++ src/test/java/br/com/alura/forum/ForumTestsBase.java
@@ -0,0 +1,14 @@
+package br.com.alura.forum;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class ForumTestsBase {
+}
--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,16 +1,12 @@
 package br.com.alura.forum.repository;
 
+import br.com.alura.forum.ForumTestsBase;
 import br.com.alura.forum.modelo.Curso;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
-public class CursoRepositoryTest {
+public class CursoRepositoryTest extends ForumTestsBase {
 
     @Autowired
     private CursoRepository repository;
